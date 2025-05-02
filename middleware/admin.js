8dd3720e2094a12ac7c93e775c47ec8b5b2ce39f
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-02 10:48:35 +0200

    first commit

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,20 +1,8 @@
 module.exports = function (req, res, next) {
   if (!req.user || req.user.role !== "admin") {
-  /*
-  Kontrollerar om  användaren inte är autentiserad).
-  Om användaren inte har rollen "admin".
-  */
-
     return res.status(404).json({ message: "Not Found" });
-  /*
-  Svarar med 404 Not Found, döljer sidan för icke-admins.
-  */
-
   }
   next();
-  /*
-  Om användaren är en admin, fortsätter vi till nästa middleware.
-  */
 };
 
 
