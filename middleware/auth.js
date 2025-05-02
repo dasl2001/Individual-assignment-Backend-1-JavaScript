aef9c435a119753ad60d7c46df4128d100919083
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-02 10:49:06 +0200

    first commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,4 @@
 const jwt = require("jsonwebtoken");
-
-
-/*
-Hämtar token från Authorization-headern.
-Verifierar token med hemlig nyckel (JWT_SECRET).
-Om token är okej sparar användarinfo i req.user och går vidare (next()).
-Om token saknas eller är fel så svarar vi med 404 Not Found.
-*/
 module.exports = (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
